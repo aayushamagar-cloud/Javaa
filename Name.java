d80de2a1fda9940282639584191f4379dd96a0da
Goma <aayusharantija@gmail.com>
2026-06-08 22:14:06 +0545

    Added arithmetic calculator program

--- Name.java
+++ Name.java
@@ -1,48 +1,64 @@
+import javax.swing.*; 
+import java.awt.*;
+import java.awt.event.*;
 
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
-public class Name {
+public class abc {
     public static void main(String[] args) throws Exception {
-       JFrame frame = new JFrame("Menu");
-       frame.setSize(400,400);
-       frame.setVisible(true);
+        JFrame frame = new JFrame("Menu");
+        frame.setSize(400,400);
+        frame.setVisible(true);
 
-       JPanel mainPanel = new JPanel();
-       frame.add(mainPanel);
+        JPanel mainPanel= new JPanel();
+        frame.add(mainPanel);
 
-       GridBagLayout gridBagLayout = new GridBagLayout();
-       mainPanel.setLayout(gridBagLayout);
+        GridBagLayout gridBagLayout = new GridBagLayout();
+        mainPanel.setLayout(gridBagLayout);
 
-       JLabel firstNameLabel = new JLabel("First Name");
-        JLabel lastNameLabel = new JLabel("Last Name");
+        JLabel firstNameLabel = new JLabel("First Name:");
+        JLabel lastNameLabel = new JLabel("Last Name:");
 
         JTextField firstNameTextField = new JTextField(15);
         JTextField lastNameTextField = new JTextField(15);
 
-        JButton button = new JButton("FULL NAME");
-       GridBagConstraints gridBagConstraints = new GridBagConstraints();
+        JButton button = new JButton("Full name");
+        GridBagConstraints gridBagConstraints = new GridBagConstraints();
+
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 0;
+        mainPanel.add(firstNameLabel, gridBagConstraints);
+
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 1;
+        mainPanel.add(firstNameTextField, gridBagConstraints);
+
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 0;
+        mainPanel.add(lastNameLabel, gridBagConstraints);
+
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 1;
+        mainPanel.add(lastNameTextField, gridBagConstraints);
+
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 2;
+        mainPanel.add(button, gridBagConstraints);
+
+        button.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+
+            String firstName = firstNameTextField.getText();
+            String lastName = lastNameTextField.getText();
 
 
-       gridBagConstraints.gridx = 0;
-       gridBagConstraints.gridy = 0;
-       mainPanel.add(firstNameLabel,gridBagConstraints);
+            if(firstName.isEmpty() || lastName.isEmpty()){
+                JOptionPane.showMessageDialog(mainPanel, "Please insert name","Empty Name", 0);
+              } else {
+                JOptionPane.showMessageDialog(mainPanel, "Full Name Hello " + firstName + " " + lastName, "Name", 0);
+            }
 
-         gridBagConstraints.gridx = 1;
-       gridBagConstraints.gridy = 0;
-       mainPanel.add(firstNameTextField,gridBagConstraints);
-
-       gridBagConstraints.gridx = 0;
-       gridBagConstraints.gridy = 1;
-       mainPanel.add(lastNameLabel,gridBagConstraints);
-
-         gridBagConstraints.gridx = 1;
-       gridBagConstraints.gridy = 1;
-       mainPanel.add(lastNameTextField,gridBagConstraints);
+        }
+        });
 
     }
 }
